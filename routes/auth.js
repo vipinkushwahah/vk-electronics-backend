e1af15214e963798ac7aa92e1f725b9c357a40d3
Vipin Kushwaha <vipinkushwaha@Vipins-MacBook-Air.local>
2025-02-21 19:01:04 +0530

    updated-pakages

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs'); // Use bcryptjs instead of bcrypt
 const User = require("../models/user");
 const router = express.Router();
 
